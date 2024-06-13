a64d24cebd9feb25a875eea765bdc5b6b534681a
Ilya Lyudevig <lyudevig@gmail.com>
2024-06-13 17:43:54 +0300

    Refactor alert test

--- cypress/e2e/project-alert.cy.ts
+++ cypress/e2e/project-alert.cy.ts
@@ -3,16 +3,15 @@ import mockProjects from "../fixtures/projects.json";
 describe("Project Alert", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000/dashboard");
-  });
-
-  it("renders alert component when request fails", () => {
     cy.intercept("GET", "https://prolog-api.profy.dev/project", {
       statusCode: 500,
     }).as("failRequest");
 
     cy.wait("@failRequest");
     cy.wait(8000);
+  });
 
+  it("renders alert component when request fails", () => {
     cy.get("main")
       .find("p")
       .contains("There was a problem while loading the projects data")
@@ -20,13 +19,6 @@ describe("Project Alert", () => {
   });
 
   it("renders project cards after clicking 'Try again' button", () => {
-    cy.intercept("GET", "https://prolog-api.profy.dev/project", {
-      statusCode: 500,
-    }).as("failRequest");
-
-    cy.wait("@failRequest");
-    cy.wait(8000);
-
     cy.get("button").contains("Try again").click();
 
     cy.intercept("GET", "https://prolog-api.profy.dev/project", {
