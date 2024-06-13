4c22fc6bdfcb1fffe0c06f7e5a28529332fa4443
Ilya Lyudevig <lyudevig@gmail.com>
2024-06-13 17:27:25 +0300

    Show error message when the request fails

---
+++ cypress/e2e/project-alert.cy.ts
@@ -0,0 +1,41 @@
+import mockProjects from "../fixtures/projects.json";
+
+describe("Project Alert", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/dashboard");
+  });
+
+  it("renders alert component when request fails", () => {
+    cy.intercept("GET", "https://prolog-api.profy.dev/project", {
+      statusCode: 500,
+    }).as("failRequest");
+
+    cy.wait("@failRequest");
+    cy.wait(8000);
+
+    cy.get("main")
+      .find("p")
+      .contains("There was a problem while loading the projects data")
+      .should("be.visible");
+  });
+
+  it("renders project cards after clicking 'Try again' button", () => {
+    cy.intercept("GET", "https://prolog-api.profy.dev/project", {
+      statusCode: 500,
+    }).as("failRequest");
+
+    cy.wait("@failRequest");
+    cy.wait(8000);
+
+    cy.get("button").contains("Try again").click();
+
+    cy.intercept("GET", "https://prolog-api.profy.dev/project", {
+      statusCode: 200,
+      body: mockProjects,
+    }).as("getProjects");
+
+    cy.wait("@getProjects");
+
+    cy.get("main").find("li").should("have.length", mockProjects.length);
+  });
+});
