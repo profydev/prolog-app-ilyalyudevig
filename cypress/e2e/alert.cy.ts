aa52ed3e55d58401ea39af21d806fe7aa1c57644
Ilya Lyudevig <lyudevig@gmail.com>
2024-07-19 01:14:17 +0300

    Implement alert component tests

--- cypress/e2e/alert.cy.ts
+++ cypress/e2e/alert.cy.ts
@@ -2,35 +2,35 @@ import mockProjects from "../fixtures/projects.json";
 
 describe("Alert", () => {
   beforeEach(() => {
+    cy.intercept(
+      { url: "https://prolog-api.profy.dev/project" },
+      {
+        statusCode: 500,
+      },
+    );
+
     cy.visit("http://localhost:3000/dashboard");
-
-    cy.intercept("GET", "https://prolog-api.profy.dev/project", {
-      forceNetworkError: true,
-    }).as("failRequest");
-
-    cy.reload();
-
-    cy.wait("@failRequest");
-    cy.wait(5000);
   });
-
   it("renders alert component when request fails", () => {
     cy.get("main")
-      .find("p")
+      .find("p", { timeout: 15000 })
       .contains("There was a problem while loading the projects data")
       .should("be.visible");
   });
 
   it("renders project cards after clicking 'Try again' button", () => {
-    cy.get("button").contains("Try again").click();
+    cy.get("main")
+      .find("button", { timeout: 15000 })
+      .contains("Try again")
+      .click();
 
     cy.intercept("GET", "https://prolog-api.profy.dev/project", {
       statusCode: 200,
       body: mockProjects,
-    }).as("getProjects");
+    }).as("successfulRequest");
 
-    cy.wait("@getProjects");
+    cy.wait("@successfulRequest");
 
-    cy.get("main").find("li").should("have.length", mockProjects.length);
+    cy.get("main").find("li").should("have.length", "3");
   });
 });
