3e30e6c81ead9437a6a070f41304485b6703ac87
Ilya Lyudevig <lyudevig@gmail.com>
2024-10-28 22:34:45 +0200

    Implement landing page header (#26)

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,6 @@
 import { Routes } from "@config/routes";
 import styles from "./index.module.scss";
+import { ButtonCTA, ButtonCTASize } from "@features/ui";
 
 const IssuesPage = () => {
   return (
@@ -7,7 +8,28 @@ const IssuesPage = () => {
       <header className={styles.header}>
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img src="/icons/logo-large.svg" alt="Prolog logo" />
-        <a href={Routes.projects}>Dashboard</a>
+        <div className={styles.navLinks}>
+          <a href={Routes.home} className={styles.navLink}>
+            Home
+          </a>
+          <a href={Routes.products} className={styles.navLink}>
+            Products
+          </a>
+          <a href={Routes.documentation} className={styles.navLink}>
+            Documentation
+          </a>
+          <a href={Routes.pricing} className={styles.navLink}>
+            Pricing
+          </a>
+        </div>
+        <ButtonCTA
+          size={ButtonCTASize.Medium}
+          onClick={() => {
+            window.open(Routes.projects, "_self");
+          }}
+        >
+          Open Dashboard
+        </ButtonCTA>
       </header>
       <button
         className={styles.contactButton}
